33601b01cc4fa1a322e0f2c578982898a3045d45
Your Name <you@example.com>
2023-12-07 04:18:49 +0100

    refactori 73

--- chatgpt-demo/src/main/java/com/howtodoinjava/demo/ChatGptApplication.java
+++ chatgpt-demo/src/main/java/com/howtodoinjava/demo/ChatGptApplication.java
@@ -1,5 +1,6 @@
 package com.howtodoinjava.demo;
 
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,3 +11,32 @@ public class ChatGptApplication {
     SpringApplication.run(ChatGptApplication.class, args);
   }
 }
+*/
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptApplication {
+
+  private static final Logger log = LoggerFactory.getLogger(ChatGptApplication.class);
+
+  @Value("${openai.api.key}")
+  private String openaiApiKey;
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChatGptApplication.class, args);
+  }
+
+  @PostConstruct
+  public void postConstruct() {
+    System.out.println("\n\n\n\n\n\n\n\n\n\nOpenAI API Key: " + openaiApiKey + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    log.debug("OpenAI API Key: {}", openaiApiKey);
+  }
+
+}
+
